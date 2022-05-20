3d76bb76bad31999fd5629904a7ba64211fd0058
Jonatas Correia <scjonatas@gmail.com>
2022-05-20 17:40:26 -0300

    jonatas

--- test.ts
+++ test.ts
@@ -3,18 +3,23 @@ import app from './server'
 import 'dotenv/config'
 import { Client } from 'pg';
 
-let database;
+let database: Client;
 
-beforeAll(() => {
+beforeAll(async () => {
     database = new Client({
         user: process.env.DATABASE_USERNAME,
         host: process.env.DATABASE_HOST,
         database: process.env.DATABASE_NAME,
         password: process.env.DATABASE_PASSWORD,
         port: 5432,
-      })
-      database.connect()
+    })
+    await database.connect()
 
+    await database.query('CREATE TABLE user (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, document VARCHAR(14) UNIQUE NOT NULL)');
+})
+
+afterAll(async () => {
+    await database.end()
 })
 
 describe("API", () => {
@@ -26,7 +31,7 @@ describe("API", () => {
             .post("/users")
             .send({ name: "John Doe", document: "40228922" })
             .expect(201)
-            .end(function(err, res) {
+            .end(function (err, res) {
                 if (err) return done(err);
                 return done();
             });
@@ -38,7 +43,7 @@ describe("API", () => {
             .post("/users")
             .send({ name: "John Doe", document: "40228922" })
             .expect(201)
-            .end(function(err, res) {
+            .end(function (err, res) {
                 if (err) return done(err);
                 return done();
             });
